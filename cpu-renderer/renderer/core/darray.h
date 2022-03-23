f57cda9f17253a78584f1324e3538463455b1e1a
Julius Häger <julius_hager@hotmail.com>
2022-03-23 15:37:55 +0100

    Migrated project to cpp

--- cpu-renderer/renderer/core/darray.h
+++ cpu-renderer/renderer/core/darray.h
@@ -1,10 +1,10 @@
 #ifndef DARRAY_H
 #define DARRAY_H
 
-#define darray_push(darray, value)                                          \
-    do {                                                                    \
-        (darray) = darray_hold((darray), 1, sizeof(*(darray)));             \
-        (darray)[darray_size(darray) - 1] = (value);                        \
+#define darray_push(darray, value, type)                                           \
+    do {                                                                           \
+        (darray) = (type)darray_hold((void*)(darray), 1, sizeof(*(darray)));       \
+        (darray)[darray_size((void*)darray) - 1] = (value);                        \
     } while (0)
 
 void *darray_hold(void *darray, int count, int item_size);
