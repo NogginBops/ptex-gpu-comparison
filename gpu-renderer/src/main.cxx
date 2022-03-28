b28e084af0a1400f92f1697972c7054235cc99df
Julius Häger <julius_hager@hotmail.com>
2022-03-28 17:48:02 +0200

    Changed the gpu renderer to render a color triangle (and do the same for the cpu renderer). Change so both projects use the space key to take screenshots

--- gpu-renderer/src/main.cxx
+++ gpu-renderer/src/main.cxx
@@ -24,7 +24,7 @@ bool takeScreenshot = false;
 
 void GLFWKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_P)
+    if (key == GLFW_KEY_SPACE)
     {
         takeScreenshot = true;
     }
@@ -99,16 +99,20 @@ int main(int argv, char** argc)
 
     const char* vertexShaderSource = "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
+        "out vec3 fColor;\n"
         "void main()\n"
         "{\n"
+        "   const vec3 colors[3] = { {1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0} };\n"
+        "   fColor = colors[gl_VertexID];\n"
         "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
         "}\0";
 
     const char* fragmentShaderSource = "#version 330 core\n"
+        "in vec3 fColor;\n"
         "out vec4 FragColor;\n"
         "void main()\n"
         "{\n"
-        "   FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n"
+        "   FragColor = vec4(fColor, 1.0);\n"
         "}\0";
 
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
