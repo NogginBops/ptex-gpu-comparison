bf4f1d813b09531047a0c75b49d957c03d5781ad
Hugo Dettner Källander <hugodk@kth.se>
2022-05-26 18:05:03 +0200

    Add automatic comparisons script

--- comparison/runcomparisons.sh
+++ comparison/runcomparisons.sh
@@ -3,17 +3,28 @@ declare -a metrics=("ssim" "psnr" "mse" "nrmse")
 declare -a scenarios=("teapotside" "teapotabove" "teapotzoomed" "groundhorizon" "groundzoomed" "groundabove")
 declare -a othermethods=("Discard" "Traverse")
 
-# Tables
+mkdir -p out/plots
+mkdir -p out/images
+mkdir -p out/tables
 
+# Tables
+for metric in "${metrics[@]}"
+do
+  python compare.py table in/Traverse -g Discard in/Discard -g Hybrid in/Hybrid -m "$metric" -o out/tables/Traverse-"$metric".tex
+done
+python compare.py table in/Traverse -g Discard in/Discard -g Hybrid in/Hybrid -v metricsavg -o out/tables/average.tex
 
 # Images
-for 
-	for scenario in "${scenarios[@]}" do
-		python compare.py image Intel/teapotside.png Hybrid/teapotside.png -m ssim pixeldiff diffcontours -s -n Intel Hybrid -o all.png
+for othermethod in "${othermethods[@]}"
+do
+	for scenario in "${scenarios[@]}"
+	do
+		python compare.py image in/"$othermethod"/"$scenario".png in/Hybrid/"$scenario".png -m ssim pixeldiff diffcontours -s -n "$othermethod" Hybrid -o out/images/"$othermethod"-Hybrid-"$scenario".png
 	done
+done
 
 # Plots
-
-for metric in "${metrics[@]}" do
-	python compare.py table in/Traverse -g Discard in/Discard -g Hybrid in/Hybrid -m "$metric" -o out/plots/nv-"$metric".png
+for metric in "${metrics[@]}"
+do
+	python compare.py table in/Traverse -g Discard in/Discard -g Hybrid in/Hybrid -m "$metric" -p -o out/plots/nv-"$metric".png
 done
