d855c1862daed03993e27d1ec7a78d6ef831030e
Hugo Dettner Källander <hugodk@kth.se>
2022-05-30 00:50:06 +0200

    Add creation of individual image files to compare.py

--- comparison/runcomparisons.sh
+++ comparison/runcomparisons.sh
@@ -7,21 +7,22 @@ mkdir -p out/plots
 mkdir -p out/images
 mkdir -p out/tables
 
+# Images
+for othermethod in "${othermethods[@]}"
+do
+	for scenario in "${scenarios[@]}"
+	do
+	  # mkdir -p out/images/"$othermethod"-Hybrid-"$scenario"
+		python compare.py image in/"$othermethod"/"$scenario".png in/Hybrid/"$scenario".png -m ssim pixeldiff diffcontours -s -n "$othermethod" Hybrid -o out/images/"$othermethod"-Hybrid-"$scenario"-all.png
+	done
+done
+
 # Tables
 for metric in "${metrics[@]}"
 do
   python compare.py table in/Traverse -g Discard in/Discard -g Hybrid in/Hybrid -m "$metric" -o out/tables/Traverse-"$metric".tex
 done
 python compare.py table in/Traverse -g Discard in/Discard -g Hybrid in/Hybrid -v metricsavg -o out/tables/average.tex
-
-# Images
-for othermethod in "${othermethods[@]}"
-do
-	for scenario in "${scenarios[@]}"
-	do
-		python compare.py image in/"$othermethod"/"$scenario".png in/Hybrid/"$scenario".png -m ssim pixeldiff diffcontours -s -n "$othermethod" Hybrid -o out/images/"$othermethod"-Hybrid-"$scenario".png
-	done
-done
 
 # Plots
 for metric in "${metrics[@]}"
